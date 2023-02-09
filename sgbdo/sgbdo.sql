98de839a566f0c64329abb0cc49203cfbf2dc128
limainfo <limainfo@gmail.com>
2023-02-09 09:12:17 -0400

    Add files via upload

---
+++ sgbdo/sgbdo.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MaquinaLocal
+Source Server Version : 50628
+Source Host           : 127.0.0.1:3306
+Source Database       : sgbdo
+
+Target Server Type    : MYSQL
+Target Server Version : 50628
+File Encoding         : 65001
+
+Date: 2016-04-20 07:20:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
